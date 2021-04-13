642ea7396e1f0525e404a521332d1677d7c77208
GeorgiNedelev <george.nedelev@gmail.com>
2021-04-13 12:20:42 +0200

    Local data saving and classroom environment
    Local data saving and classroom environment

--- Hyperactivity screening tool/Assets/Scripts/ControllerDataCollectionScript.cs
+++ Hyperactivity screening tool/Assets/Scripts/ControllerDataCollectionScript.cs
@@ -7,6 +7,9 @@ using System.IO;
 using System;
 using UnityEngine.XR;
 
+
+
+
 public class SensorClass : MonoBehaviour
 {
 
@@ -18,6 +21,10 @@ public class SensorClass : MonoBehaviour
     public List<Vector3> Angular_Velocity_list;
     public List<Vector3> Angular_Acceleration_list;
     public List<List<Vector3>> MeasurementsCollectiveList;
+
+    public float timeStep = 1.0f;
+    public float time1 = 0.0f;
+    public int sec = 1;
     
     public SensorClass(string SensorName,InputDevice Sensor)
     {
@@ -47,16 +54,29 @@ public class SensorClass : MonoBehaviour
         //angular
         sensorDevice.TryGetFeatureValue(CommonUsages.deviceAngularVelocity, out Vector3 angularVelocity);
         sensorDevice.TryGetFeatureValue(CommonUsages.deviceAngularAcceleration, out Vector3 angularAcceleration);
+        // time control
+        if (time1 < Time.time)
+        {
 
-    
+
+            
+            time1 = Time.time + timeStep;
+            Debug.LogWarning("Inputs in a second" + Position_list.Count / Time.time);
+            
+            //Debug.LogWarning("Inputs " + Position_list.Count );
+            //Debug.LogWarning(" second :" +  sec);
+            sec ++;
+        }
         SensorSaveDataToList(Position_list, devicePosition);
         SensorSaveDataToList(Velocity_list, deviceVelocity);
         SensorSaveDataToList(Acceleration_list, deviceAcceleration);
         SensorSaveDataToList(Angular_Acceleration_list, angularVelocity);
         SensorSaveDataToList(Angular_Velocity_list, angularAcceleration);
 
-
+        
     }
+
+    
 
     //Adding data to a sensor parameter list
     public void SensorSaveDataToList(List<Vector3> dataList, Vector3 dataToSave)
@@ -64,19 +84,24 @@ public class SensorClass : MonoBehaviour
         if (dataList.Count > 0)
         {
 
-            if (dataList[dataList.Count - 1].ToString() != dataToSave.ToString())
-            {
+            // if (dataList[dataList.Count - 1].ToString() != dataToSave.ToString())
 
+            
                 dataList.Add(dataToSave);
-                Debug.LogWarning("Added Value" + dataToSave);
+                //Debug.LogWarning("Added Value" + dataToSave);
+
                 // Debug.LogWarning("last data point :" + RightControllerData_Acceleration[RightControllerData_Acceleration.Count - 1] + "added data point" + devicePosition );
-            }
-            //Debug.LogWarning("Discarded value" + dataToSave);
+
+                //Debug.LogWarning("Discarded value" + dataToSave);
+               
+            
         }
         else
         {
             dataList.Add(dataToSave);
         }
+
+        
 
     }
     
@@ -91,11 +116,12 @@ public class SensorClass : MonoBehaviour
 
         for (int i = 0; i < MeasurementsCollectiveList.Count; i++)
         {
-
-        string strFilePath = (@"C:\Data\" + sensorName + listNames[i]+ DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".csv");
-        string strSeperator = ",";
+        string path = Application.persistentDataPath ;
+        string strFilePath = ( path + sensorName + listNames[i]+ DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + ".csv");
+            //@"C:\Data\"
+            string strSeperator = ",";
         StringBuilder sbOutput = new StringBuilder();
-        //UnicontaAPI.FetchInv(inventar, compID, userN, pw).GetAwaiter().GetResult();
+        ;
         sbOutput.AppendLine(string.Join(strSeperator, "X", "Y", "Z"));
         foreach (Vector3 input in MeasurementsCollectiveList[i])
                {
